a6cf6c8f6119366b2673f35086142307781917cf
jojimanos <manosgeorge7@gmail.com>
2025-03-30 18:24:19 +0300

    Integrated rate-repository-api

---
+++ rate-repository-app/src/hooks/useRepositories.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+function useRepositories(url: string) {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    const response = await fetch(url);
+    const json = await response.json();
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+}
+
+export default useRepositories;
